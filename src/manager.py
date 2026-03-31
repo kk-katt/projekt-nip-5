cef3474f1cbddb12b96a3da8c939a1fb9ac558cb
Katarzyna <kuleczka779@wp.pl>
2026-03-31 14:46:54 +0300

    część dodania rozliczenia dla mieszkania oraz test

--- src/manager.py
+++ src/manager.py
@@ -1,4 +1,4 @@
-from src.models import Apartment, Bill, Parameters, Tenant, Transfer
+from src.models import Apartment, Bill, Parameters, Tenant, Transfer, ApartmentSettlement
 
 
 class Manager:
@@ -24,28 +24,27 @@ class Manager:
                 return False
         return True
     
-    def get_apartment_costs(self, apartment_key, year, month):
+    def get_apartment_costs(self, apartment_key, year=None, month=None):
+        if apartment_key not in self.apartments:
+            return None
         total = 0
         for bill in self.bills:
             if apartment_key != bill.apartment:
                 continue
-            if month != bill.settlement_month:
+            if year != bill.settlement_year and year != None:
+                continue
+            if month != bill.settlement_month and month != None:
                 continue
             total += bill.amount_pln
         return total
     
-    # class Bill(BaseModel):
-    # amount_pln: float
-    # date_due: str
-    # apartment: str
-    # settlement_year: int
-    # settlement_month: int
-    # type: str
+    def get_settlement_costs(self, apartment_key, year, month):
+        settlement = ApartmentSettlement(
+            apartment = apartment_key,
+            year = year,
+            month = month,
+            total_bills_pln = self.get_apartment_costs(apartment_key, year, month),
+            #total_rent_pln = z najemcow jakos,
+            #total_due_pln = total_bills_pln + total_rent_pln,
+        )
 
-    # @staticmethod
-    # def from_json_file(file_path: str) -> List['Bill']:
-    #     data = None
-    #     with open(file_path, 'r') as file:
-    #         data = json.load(file)
-    #     assert isinstance(data, list), "Expected a list of bills"
-    #     return [Bill(**bill) for bill in data]
